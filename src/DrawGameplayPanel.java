06b1a0efd0290d6dd59a3c8d5f771a064c69e365
BT_4N2_18 <onl@jhjlhj>
2024-04-18 13:04:57 -0400

    Managed to make the gas can sprite appear on screen for the second panel!

--- src/DrawGameplayPanel.java
+++ src/DrawGameplayPanel.java
@@ -1,24 +1,31 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class DrawGameplayPanel extends JPanel implements MouseListener {
+    private BufferedImage image;
 
 
     public DrawGameplayPanel() {
         this.addMouseListener(this);
+        try{
+            image = ImageIO.read(new File("Sprites/GasCan.png"));
+        } catch (IOException e){
+            System.exit(1);
+        }
 
-    }
-
-    public DrawGameplayPanel(String newGame) {
-        this.addMouseListener(this);
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setOpaque(true);
         this.setBackground(Color.BLACK);
+        g.drawImage(image, 0, 0, this);
 
     }
 
